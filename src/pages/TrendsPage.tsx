e843ae1f710887156320c29d82d31d8ba6084298
[dyad] <alex_rendon@live.com>
2025-12-18 14:41:55 -0600

    [dyad] Removed the duplicate header from TrendsPage. - wrote 1 file(s)

--- src/pages/TrendsPage.tsx
+++ src/pages/TrendsPage.tsx
@@ -5,9 +5,6 @@ const TrendsPage: React.FC = () => {
   return (
     <div className="min-h-[calc(100vh-100px)] p-4 sm:p-8 bg-black text-white">
       <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-bold mb-12 text-center">
-          Live Trend Discovery
-        </h1>
         <ViralPostList />
       </div>
     </div>
