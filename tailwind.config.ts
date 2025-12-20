16f6d93ec7699976fbdcf7091690bf25aa2a5e36
[dyad] <alex_rendon@live.com>
2025-12-20 15:34:55 -0600

    [dyad] Fixing Tailwind color configuration - wrote 1 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,8 +66,8 @@ export default {
         brand: {
           primary: "#E86324",
           hover: "#FF8A3A",
-          "secondary-blue": "#f9cf29",
-          "secondary-yellow": "#5fb0fd",
+          "secondary-blue": "#5fb0fd", // Blue
+          "secondary-yellow": "#f9cf29", // Yellow
         },
       },
       borderRadius: {
